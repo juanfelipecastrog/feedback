6093f57d670bca88539f1ba937ca3c66bbc933c3
Juan Felipe <juanfelipe.castro@endava.com>
2023-07-13 11:44:10 -0500

    Fixing CORS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"api/controller"
 	"api/data"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,10 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	r.Use(cors.New(config))
 	r.GET("/health-check/", controller.HealthCheck)
 	r.GET("/feedback/", controller.ShowFeedbacks)
 	r.GET("/status/", controller.StatusSummary)
