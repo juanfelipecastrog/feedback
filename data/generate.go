e9554c4f2172f2edc69fd6ad647bfd560d56c06a
Juan Felipe <juanfelipe.castro@endava.com>
2023-07-13 14:02:53 -0500

    Adding names for LM

--- data/generate.go
+++ data/generate.go
@@ -16,7 +16,7 @@ func GenerateFeedbacks(quantity int) []model.Feedbacks {
 		feedback := model.Feedbacks{
 			ID:                   fmt.Sprintf("%d", i),
 			Name:                 fmt.Sprintf("Employee %d", i),
-			LineManager:          fmt.Sprintf("Manager %d", i),
+			LineManager:          getRandomManager(),
 			CareerCoach:          fmt.Sprintf("Coach %d", i),
 			Authorized:           fmt.Sprintf("Authorized %d", i),
 			CompensationReviewer: fmt.Sprintf("Reviewer %d", i),
@@ -43,14 +43,75 @@ func getRandomStatus() string {
 }
 
 func getRandomPeriod() string {
-	quarters := []string{"H1 FY2023", "H2 FY2023", "H2 FY2022", "H1 FY2022"}
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	quarter := quarters[r.Intn(len(quarters))]
-	return quarter
+	chooser, _ := weightRand.NewChooser(
+		weightRand.Choice{Item: "H1 FY2023", Weight: 3},
+		weightRand.Choice{Item: "H2 FY2023", Weight: 1},
+		weightRand.Choice{Item: "H2 FY2022", Weight: 4},
+		weightRand.Choice{Item: "H1 FY2022", Weight: 6},
+	)
+	period := chooser.Pick().(string)
+	return period
 }
 
 func getRandomDiscipline() string {
 	disciplines := []string{"Development", "AM", "Testing", "Creative", "Facilities"}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return disciplines[r.Intn(len(disciplines))]
+}
+
+func getRandomManager() string {
+	chooser, _ := weightRand.NewChooser(
+		weightRand.Choice{Item: "Sophie", Weight: 2},
+		weightRand.Choice{Item: "Liam", Weight: 2},
+		weightRand.Choice{Item: "Olivia", Weight: 2},
+		weightRand.Choice{Item: "Noah", Weight: 2},
+		weightRand.Choice{Item: "Emma", Weight: 2},
+		weightRand.Choice{Item: "Jackson", Weight: 2},
+		weightRand.Choice{Item: "Ava", Weight: 2},
+		weightRand.Choice{Item: "Lucas", Weight: 2},
+		weightRand.Choice{Item: "Isabella", Weight: 2},
+		weightRand.Choice{Item: "Aiden", Weight: 2},
+		weightRand.Choice{Item: "Mia", Weight: 2},
+		weightRand.Choice{Item: "Caden", Weight: 2},
+		weightRand.Choice{Item: "Charlotte", Weight: 2},
+		weightRand.Choice{Item: "Mason", Weight: 2},
+		weightRand.Choice{Item: "Amelia", Weight: 2},
+		weightRand.Choice{Item: "Oliver", Weight: 2},
+		weightRand.Choice{Item: "Harper", Weight: 2},
+		weightRand.Choice{Item: "Elijah", Weight: 2},
+		weightRand.Choice{Item: "Evelyn", Weight: 2},
+		weightRand.Choice{Item: "Grayson", Weight: 2},
+		weightRand.Choice{Item: "Abigail", Weight: 2},
+		weightRand.Choice{Item: "Jacob", Weight: 2},
+		weightRand.Choice{Item: "Emily", Weight: 2},
+		weightRand.Choice{Item: "Michael", Weight: 2},
+		weightRand.Choice{Item: "Elizabeth", Weight: 2},
+		weightRand.Choice{Item: "Benjamin", Weight: 2},
+		weightRand.Choice{Item: "Ella", Weight: 2},
+		weightRand.Choice{Item: "Carter", Weight: 2},
+		weightRand.Choice{Item: "Sofia", Weight: 2},
+		weightRand.Choice{Item: "James", Weight: 2},
+		weightRand.Choice{Item: "Avery", Weight: 2},
+		weightRand.Choice{Item: "Alexander", Weight: 2},
+		weightRand.Choice{Item: "Grace", Weight: 2},
+		weightRand.Choice{Item: "Matthew", Weight: 2},
+		weightRand.Choice{Item: "Chloe", Weight: 2},
+		weightRand.Choice{Item: "Wyatt", Weight: 2},
+		weightRand.Choice{Item: "Scarlett", Weight: 2},
+		weightRand.Choice{Item: "Luke", Weight: 2},
+		weightRand.Choice{Item: "Victoria", Weight: 2},
+		weightRand.Choice{Item: "Henry", Weight: 2},
+		weightRand.Choice{Item: "Luna", Weight: 2},
+		weightRand.Choice{Item: "Andrew", Weight: 2},
+		weightRand.Choice{Item: "Penelope", Weight: 2},
+		weightRand.Choice{Item: "Daniel", Weight: 2},
+		weightRand.Choice{Item: "Layla", Weight: 2},
+		weightRand.Choice{Item: "Logan", Weight: 2},
+		weightRand.Choice{Item: "Zoey", Weight: 2},
+		weightRand.Choice{Item: "William", Weight: 2},
+		weightRand.Choice{Item: "Nora", Weight: 2},
+		weightRand.Choice{Item: "Gabriel", Weight: 2},
+	)
+	manager := chooser.Pick().(string)
+	return manager
 }
